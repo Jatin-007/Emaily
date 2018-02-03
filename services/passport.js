d8b40f9c30d15df3ca296e1173d4c641b5a960c9
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-03 07:19:04 -0800

    tweaking google strategy

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,7 @@ passport.use
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
             callbackURL: '/auth/google/callback', // providing a callback after user grants the permission,
-            Proxy: true
+            proxy: true
         },
 
         (accessToken, refreshToken, profile, done)=> {
