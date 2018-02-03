ba12b145c6973abef820fb0f5f9b160a9912b2b2
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-03 07:02:57 -0800

    Finished Auth flow

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// keys.js - figure out the set of credentials to return
+
+
+if(process.env.NODE_ENV === 'production') {
+    // we are in production - return the prod keys
+    module.exports = require ('./prod');
+}
+
+else {
+    // in development !! return the dev keys !
+    module.exports = require ('./dev');
+}
+
+ 
