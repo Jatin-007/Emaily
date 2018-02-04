616904381a798bfe173415fa91bab9593ad6c376
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-02-04 04:40:37 -0800

    Added Redux to the project ||Boilerplate?

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = ()=> {
+    return (
+        <div>
+            Hi There!
+        </div>
+    );
+};
+
+export default App;
+
+// we are using import at the front end as webpack and babel provides ES2015 syntax
+// for Node.js, it only provides the common syntax. Hence, we are using require at backend
