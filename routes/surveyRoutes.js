31c1d78b60b07a8dd65db11a7096cd02ac665cfc
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-15 15:31:43 -0700

    Updating surveyRoutes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,26 @@
+const mongoose = require ('mongoose');
+
 const requireLogin = require ('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         // checking up if the user is logged in or no in the app by using the requireLogin
+    // requesting title, subject, body and recipients
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey ({
+        title,
+        subject,
+        body,
+        recipients: recipients.split(',').map(email => ({ email })),
+        // getting the recipients in a well managed way.
+        // split method is splitting the emails wherever , is located and storing emails in a seperate object
+        _user: req.user.id,
+        dateSent: Date.now() // providing the date the survey is sent
+    });
 
     });
 };
