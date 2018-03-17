44c03ee1ab7cc58979a83734be7ed904844abfb2
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-17 08:50:17 -0700

    Setting up Mailer for email services

--- services/Mailer.js
+++ services/Mailer.js
@@ -5,7 +5,43 @@ const keys = require('../config/keys');
 
 // inheriting mail object inside mailer class
 class Mailer extends helper.Mail {
-    
+    constructor({ subject, recipients }, content) {
+        super();
+        // helper function is provided by sendgrid 
+        this.from_email = new helper.Email('np-reply@emaily.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+
+        this.addContent(this.body); // addContent is a defined function inside helper.Mail which we are extending
+        this.addClickTracking();
+        this.addRecipients();
+    }
+
+    formatAddresses(recipients){
+        // mapping up each email and formatting them in the correct way
+        return recipients.map(({ email }) => {
+            return new helper.Email(email); // formatiing email with the help of helper.Email which formats them in a specific way
+        });
+    }
+
+    addClickTracking() {
+        const trackingSettings = new helper.TrackingSettings();
+        const clickTracking = new helper.ClickTracking (true, true);
+
+        trackingSettings.setClickTracking(clickTracking);
+        this.addTrackingSettings(trackingSettings);
+    }
+
+    addRecipients() {
+        const personalize = new helper.Personalization();
+        this.recipients.forEach(recipient => {
+            personalize.addTo(recipient);
+        });
+        this.addPersonalization(personalize);
+    }
 }
 
+// whenever we create a new statement (which we did new surveyROutes)
+// Constructor is automatically called...
 module.exports = Mailer;
