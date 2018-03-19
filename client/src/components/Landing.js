622232ed3ff4e0d9c6f3d3a9826f005dc55963c3
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-19 13:17:56 -0700

    very minor changes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -7,14 +7,10 @@ const Landing = ()=> {
             <h1>
                 Emaily!!!
             </h1>
-            <div className= "z-depth-1" style={{backgroundColor: '#168e76', color: 'white', padding: 12}}>
+            <div className= "z-depth-4" style={{backgroundColor: '#168e76', color: 'white', padding: 12, borderLeft: '2px solid black', borderRight: '2px solid black'}}>
                 <p>
                     <ReactTyped/>
                 </p>
-            </div>
-
-            <div>
-
             </div>
         </div>
     );
