c3bbee5cf3bcffd9481246541488d1b024c39d88
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-23 08:16:47 -0700

    re-rendering SurveyFields

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,10 +1,11 @@
 // surveyfields contains logic to render single label and text input
 import React from 'react';
 
-const SurveyField = ({ input }) => {
+const SurveyField = ({ input, label }) => {
     
     return (
         <div>
+            <label>{label}</label>
             <input {...input}  />
         </div>
     );
--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,16 +1,23 @@
 // adding users over here
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { reduxForm, Field } from 'redux-form'; // helps to communicate with the redux store to transfer the information
 import SurveyField from './SurveyField';
+
+
+const FIELDS = [
+    {label: 'Survey Title', name:'Title'},
+    {label: 'Subject Line', name: 'subject'},
+    {label: 'Email Body', name:'body'},
+    {label: 'Recipient List', name:'emails'},
+];
 
 class SurveyForm extends Component {
 
     renderFields() {
-        return (
-            <div>
-                <Field type="text" name="title" component = {SurveyField} />
-            </div>
-        );
+        return _.map(FIELDS, ({label, name})=> {
+            return <Field key={name} component={SurveyField} type="text" label={label} name={name} />
+        });
     }
 
     render () {
