0288dbc492c3821ff22591a98be5c73052170ea1
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-03-24 17:21:22 -0700

    Minor changes

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,9 +1,12 @@
+// Survey form review shows users their form review after adding the values
 import React from 'react';
 
-const SurveyFormReview = () => {
+const SurveyFormReview = ({ onCancel }) => {
     return (
         <div>
             <h6>Please confirm your entries</h6>
+
+            <button className="yellow darken-3 btn-flat" onClick={onCancel}>Back</button>
         </div>
     );
 };
