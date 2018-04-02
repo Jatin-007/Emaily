3c674936ed13764cceff95bd9ff1286b5a162c35
Jatin Suneja <jatinsuneja1903@gmail.com>
2018-04-02 09:54:48 -0700

    Re-rendering webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -16,33 +16,9 @@ module.exports = app => {
         res.send('Thanks for your feedback');
     });
     
+    //////////// 
     app.post('/api/surveys/webhooks',(req, res) => {
-        const p = new Path('/api/surveys/:surveyId/:choice');
-        
-        _.chain(req.body)
-        .map(({email, url})=> {
-            const match = p.test(new URL(url).pathname);
-            if(match){
-                return { email, surveyId: match.surveyId, choice: match.choice};
-            }
-        })
-        .compact()// will show all the events and wont display the undefined objects
-        .uniqBy('email', 'surveyId')// will filter out any duplicate email and surveyId
-        .each(({surveyId, email,choice }) => {
-            Survey.updateOne({
-                //_id is how we use for mongoDB
-                _id: surveyId,
-                recipients: {
-                    $elemMatch : { email: email, responded: false }
-                }
-            }, {
-                $inc: { [choice]: 1},
-                $set: {'recipients.$.responded': true},
-                lastResponded: new Date()
-            }).exec();
-        })
-        .value();
-        
+        console.log(req.body);
         res.send({});
     });
 
